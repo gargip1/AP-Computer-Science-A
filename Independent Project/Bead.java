b0d7a17fc9bbcaac555f73804229c6c8bc4129f2
gargip1 <gargi.austin@gmail.com>
2025-12-30 18:50:10 -0600

    Create Bead.java
    Defines a specific "Bead" object with a boolean Special quality.

---
+++ Independent Project/Bead.java
@@ -0,0 +1,11 @@
+public class Bead {
+  private boolean special; //special bead trait
+  
+  public Bead (boolean isSpecial) {
+    special = isSpecial;
+  }
+
+  public boolean isSpecial() { //return specialty
+    return special;
+  }
+}
